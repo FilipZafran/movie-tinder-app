70819fdb40e3671ed62e28168a58bc028eb1391c
“baskle” <baaklee@gmail.com>
2020-11-06 19:06:04 +0100

    commit to run pull request, modiefied App.js, Pages/Friends.js and Login.js

--- src/App.js
+++ src/App.js
@@ -1,24 +1,24 @@
-import React from 'react';
-import './App.css';
-import Dashboard from './Pages/Dashboard';
-import { Route, BrowserRouter as Router, Switch } from 'react-router-dom';
-import PrivateRoute from './components/PrivateRoute';
-import Login from './Pages/Login';
+import React from "react";
+import "./App.css";
+import Dashboard from "./Pages/Dashboard";
+import { Route, BrowserRouter as Router, Switch } from "react-router-dom";
+import PrivateRoute from "./components/PrivateRoute";
+import Login from "./Pages/Login";
 
 const App = () => {
-	return (
-		<Router>
-			<Switch>
-				<PrivateRoute path="/dashboard">
-					<Dashboard />
-				</PrivateRoute>
+  return (
+    <Router>
+      <Switch>
+        <PrivateRoute path="/dashboard">
+          <Dashboard />
+        </PrivateRoute>
 
-				<Route path="/">
-					<Login />
-				</Route>
-			</Switch>
-		</Router>
-	);
+        <Route path="/">
+          <Login />
+        </Route>
+      </Switch>
+    </Router>
+  );
 };
 
 export default App;
