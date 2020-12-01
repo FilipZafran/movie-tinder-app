1c64d26856f29bd504046a51fb8dab15d1dcd608
HelenHMaher <helenharlanmaher@gmail.com>
2020-12-02 00:25:10 +0100

    redux filterSlice

--- src/Redux/filtersSlice.js
+++ src/Redux/filtersSlice.js
@@ -1,18 +1,56 @@
 import { createSlice, createAsyncThunk } from '@reduxjs/toolkit';
 import axios from 'axios';
 
-const initialState = { entities: [], loading: 'idle' };
+const initialState = { entities: {}, loading: 'idle' };
 
 const serverURL = process.env.REACT_APP_SERVER;
 
 export const fetchAllFilters = createAsyncThunk(
-  'allFilters/fetchAllFilters',
+  'filters/fetchAllFilters',
   async () => {
     try {
       const response = await axios({
         method: 'GET',
         withCredentials: true,
         url: `${serverURL}/movies/filterNames`,
+      });
+      return response.data.filters;
+    } catch (err) {
+      console.log(err);
+    }
+  }
+);
+
+export const fetchActiveFilters = createAsyncThunk(
+  'filters/fetchActiveFilters',
+  async () => {
+    try {
+      const response = await axios({
+        method: 'GET',
+        withCredentials: true,
+        url: `${serverURL}/likeTracker/filters`,
+      });
+      return response.data.filters;
+    } catch (err) {
+      console.log(err);
+    }
+  }
+);
+
+export const submitActiveFilters = createAsyncThunk(
+  'filters/submitActiveFilters',
+  async (filters) => {
+    try {
+      const response = await axios({
+        method: 'POST',
+        withCredentials: true,
+        url: `${serverURL}/likeTracker/filters`,
+        data: {
+          filters: {
+            genreFilters: filters.genreFilters,
+            timeFilters: filters.timeFilters,
+          },
+        },
       });
       return response.data;
     } catch (err) {
@@ -25,7 +63,18 @@ const filtersSlice = createSlice({
   name: 'filters',
   initialState,
   reducers: {},
-  extraReducers: {},
+  extraReducers: {
+    [fetchActiveFilters.fulfilled]: (state, action) => {
+      state.entities.activeFilters = action.payload;
+    },
+    [fetchAllFilters.fulfilled]: (state, action) => {
+      state.entities.allFilters = action.payload;
+    },
+  },
 });
 
 export default filtersSlice.reducer;
+
+export const selectAllFilters = (state) => state.filters.entities.allFilters;
+export const selectActiveFilters = (state) =>
+  state.filters.entities.activeFilters;
