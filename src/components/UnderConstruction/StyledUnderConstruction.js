96239e67b0aea9577c58c5966e42db770491851d
HelenHMaher <helenharlanmaher@gmail.com>
2020-12-07 15:09:53 +0100

    under construction component

---
+++ src/components/UnderConstruction/StyledUnderConstruction.js
@@ -0,0 +1,67 @@
+import styled from 'styled-components';
+
+export const ConstructionContainer = styled.div`
+  position: fixed;
+  width: 100vw;
+  height: 65vh;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  overflow: hidden;
+`;
+
+export const ConstructionBox = styled.div`
+  background: var(--dark-900-50);
+  width: 380px;
+  height: 200px;
+  border: 2px solid var(--dar-700);
+  border-radius: 10px;
+  display: flex;
+  flex-direction: row;
+  align-items: center;
+  justify-self: center;
+`;
+
+export const ConstructionImage = styled.div`
+  display: flex;
+  border-top-left-radius: 10px;
+  border-bottom-left-radius: 10px;
+  justify-content: center;
+  overflow: hidden;
+  width: 160px;
+  height: 200px;
+  img {
+    max-height: 100%;
+  }
+`;
+
+export const ConstructionContent = styled.div`
+  display: flex;
+  flex-direction: column;
+  margin: 10px;
+`;
+
+export const ConstructionTitle = styled.div`
+  text-align: left;
+  font-size: 25px;
+  margin-bottom: 10px;
+  color: var(--light-100);
+`;
+
+export const ConstructionText = styled.div`
+  text-align: left;
+  width: 220px;
+  font-size: 12px;
+  margin-bottom: 10px;
+`;
+export const ButtonContainer = styled.div`
+  display: flex;
+  justify-content: center;
+`;
+
+export const DemoButton = styled.div`
+  padding: 12px;
+  cursor: pointer;
+  border-radius: 10px;
+  background: var(--dark-300);
+`;
--- src/components/UnderConstruction/index.js
+++ src/components/UnderConstruction/index.js
@@ -1,81 +1,36 @@
 import React from 'react';
-import styled from 'styled-components';
+import { unwrapResult } from '@reduxjs/toolkit';
 import { useDispatch } from 'react-redux';
 import { useHistory } from 'react-router-dom';
 import profilePrime from '../../Resources/profile_prime.png';
 import { loginUser } from '../../Redux/userSlice';
-
-const ConstructionContainer = styled.div`
-  position: fixed;
-  width: 100vw;
-  height: 65vh;
-  display: flex;
-  align-items: center;
-  justify-content: center;
-  overflow: hidden;
-`;
-
-const ConstructionBox = styled.div`
-  background: var(--dark-900-50);
-  width: 380px;
-  height: 200px;
-  border: 2px solid var(--dar-700);
-  border-radius: 10px;
-  display: flex;
-  flex-direction: row;
-  align-items: center;
-  justify-self: center;
-`;
-
-const ConstructionImage = styled.div`
-  display: flex;
-  border-top-left-radius: 10px;
-  border-bottom-left-radius: 10px;
-  justify-content: center;
-  overflow: hidden;
-  width: 160px;
-  height: 200px;
-  img {
-    max-height: 100%;
-  }
-`;
-
-const ConstructionContent = styled.div`
-  display: flex;
-  flex-direction: column;
-  margin: 10px;
-`;
-
-const ConstructionTitle = styled.div`
-  text-align: left;
-  font-size: 25px;
-  margin-bottom: 10px;
-  color: var(--light-100);
-`;
-
-const ConstructionText = styled.div`
-  text-align: left;
-  width: 220px;
-  font-size: 12px;
-  margin-bottom: 10px;
-`;
-const ButtonContainer = styled.div`
-  display: flex;
-  justify-content: center;
-`;
-
-const DemoButton = styled.div`
-  padding: 12px;
-  cursor: pointer;
-  border-radius: 10px;
-  background: var(--dark-300);
-`;
+import {
+  ConstructionContainer,
+  ConstructionBox,
+  ConstructionImage,
+  ConstructionContent,
+  ConstructionText,
+  ConstructionTitle,
+  ButtonContainer,
+  DemoButton,
+} from './StyledUnderConstruction';
 
 export const UnderConstruction = () => {
   const dispatch = useDispatch();
-  const clickHandler = () => {
-    dispatch(loginUser({ username: 'Admin', password: 'password' }));
-    console.log('logged in to demo account');
+  const history = useHistory();
+  const clickHandler = async () => {
+    try {
+      const login = await dispatch(
+        loginUser({ username: 'Admin', password: 'password' })
+      );
+      unwrapResult(login);
+      if (login.payload.message === 'Successfully Authenticated') {
+        localStorage.setItem('isAuthenticated', true);
+        history.replace('/dashboard');
+      }
+    } catch (err) {
+      console.log(err);
+    }
   };
 
   return (
