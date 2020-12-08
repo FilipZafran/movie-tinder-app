a985c3a9b123f2729fd3b3288a4256565b3f230b
FichoZee <filip.zafran@gmail.com>
2020-12-08 16:40:17 +0100

    add progress bar and error messages

--- src/components/Profile/FileUpload.js
+++ src/components/Profile/FileUpload.js
@@ -1,11 +1,14 @@
 import React, { Fragment, useState } from 'react';
 import axios from 'axios';
-import { lightGreen } from '@material-ui/core/colors';
+import Message from './Message';
+import ProgressBar from './ProgressBar';
 
 const FileUpload = () => {
 	const [ file, setFile ] = useState('');
 	const [ fileName, setFileName ] = useState('Upload File');
 	const [ uploadedFile, setUploadedFile ] = useState({});
+	const [ message, setMessage ] = useState('');
+	const [ uploadPercentage, setUploadPercentage ] = useState(0);
 
 	const onChange = (e) => {
 		setFile(e.target.files[0]);
@@ -21,6 +24,12 @@ const FileUpload = () => {
 			const res = await axios.post(`${process.env.REACT_APP_SERVER}/uploads/`, formData, {
 				headers: {
 					'Content-Type': 'multipart/form-data'
+				},
+				onUploadProgress: (progressEvent) => {
+					setUploadPercentage(parseInt(Math.round(progressEvent.loaded * 100) / progressEvent.total));
+
+					// clear percentage
+					setTimeout(() => setUploadPercentage(0), 5000);
 				}
 			});
 
@@ -28,13 +37,21 @@ const FileUpload = () => {
 			console.log(fileName, filePath);
 
 			setUploadedFile({ fileName, filePath });
+
+			setMessage('File Uploaded succesfully');
 		} catch (err) {
-			console.error(err);
+			if (err) {
+				setMessage('Server Issue');
+			} else {
+				setMessage(err.response.data.msg);
+			}
 		}
 	};
 
 	return (
 		<Fragment>
+			{message ? <Message msg={message} /> : null}
+
 			<form onSubmit={onSubmit}>
 				<div className='custome-file mb-4'>
 					<input type='file' className='custome-file-input' id='customFile' onChange={onChange} />
@@ -44,8 +61,16 @@ const FileUpload = () => {
 					</label>
 				</div>
 
+				<ProgressBar percentage={uploadPercentage} />
+
 				<input type='submit' value='upload' className='btn btn-block mt-4' id='customFile' />
 			</form>
+			{uploadedFile ? (
+				<div className='row mt-5'>
+					{uploadedFile.fileName}
+					<img id='uploaded-profile-picture' src={uploadedFile.filePath} alt='my profile picture' />
+				</div>
+			) : null}
 		</Fragment>
 	);
 };
---
+++ src/components/Profile/ProgressBar.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+
+const ProgressBar = ({ percentage }) => {
+	return (
+		<div className='progress'>
+			<div className='progress-bar bg-success' role='progressbar' style={{ width: `${percentage}%` }} />
+			{percentage}%
+		</div>
+	);
+};
+
+ProgressBar.propTypes = {
+	percentage: PropTypes.number.isRequired
+};
+
+export default ProgressBar;
