b34a2d54be433cacc87cff74744ed74e57b5d811
FichoZee <filip.zafran@gmail.com>
2020-12-09 12:15:44 +0100

    circle crop profile picture

--- src/components/Profile/FileUploader.js
+++ src/components/Profile/FileUploader.js
@@ -8,7 +8,7 @@ function FileUploader() {
 		const files = e.target.files;
 		const data = new FormData();
 		data.append('file', files[0]);
-		data.append('upload_preset', 'profile_picture');
+		data.append('upload_preset', 'cloudinary_profile_pic');
 		setLoading(true);
 		const res = await fetch('https://api.cloudinary.com/v1_1/filmably-app/image/upload', {
 			method: 'POST',
@@ -22,11 +22,15 @@ function FileUploader() {
 	};
 
 	return (
-		<div>
-			<h1>Uploade Image</h1>
-
+		<div className='profile__upload-div'>
 			<input type='file' name='file' onChange={uploadImage} />
-			{loading ? <h3>Loading...</h3> : <img src={image} style={{ width: '300px' }} />}
+			{loading ? (
+				<h3>Loading...</h3>
+			) : (
+				<div class='profile__image-cropper'>
+					<img id='profile__circle_image' src={image} />{' '}
+				</div>
+			)}
 		</div>
 	);
 }
