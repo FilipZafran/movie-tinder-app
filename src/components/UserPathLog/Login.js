1082b8a3645d8922443552be146aa966365e05e0
HelenHMaher <helenharlanmaher@gmail.com>
2020-12-14 20:10:34 +0100

    update authentication route

--- src/components/UserPathLog/Login.js
+++ src/components/UserPathLog/Login.js
@@ -1,44 +1,40 @@
-import Axios from "axios";
-import React, { useState } from "react";
-import { useHistory } from "react-router-dom";
+import React, { useState } from 'react';
+import { useHistory, Link } from 'react-router-dom';
 // import Registration from '../UserPathLog/Register';
 // import Resetpw from '../UserPathLog/Resetpw';
-import { Link } from "react-router-dom";
-import "./Login.css";
-import { Logo } from "../styleElements/icons/Logo";
-import { Button } from "../styleElements/buttons/Button";
+import './Login.css';
+import { Logo } from '../styleElements/icons/Logo';
+import { Button } from '../styleElements/buttons/Button';
 
-import { UnderConstruction } from "../UnderConstruction";
+import { loginUser } from '../../Redux/userSlice';
+import { useDispatch } from 'react-redux';
+import { unwrapResult } from '@reduxjs/toolkit';
 
 export default function Login() {
-  const [username, setUsername] = useState("");
-  const [password, setPassword] = useState("");
-  const serverUrl = process.env.REACT_APP_SERVER;
+  const [username, setUsername] = useState('');
+  const [password, setPassword] = useState('');
 
   const history = useHistory();
+  const dispatch = useDispatch();
 
-  //this could be changed to an async function
   //this verification route will work if you run the server from index.js on your local machine
   //currently the credentials username:Admin password:password will work
   //a registration route is set up under POST "http://localhost:5000/authentication/register" (also accepts username and pw)
-  function login() {
-    Axios({
-      method: "POST",
-      data: {
-        username: username,
-        password: password,
-      },
-      withCredentials: true,
-      url: `${serverUrl}/authenticate/login`,
-    }).then((res) => {
-      if (res.data.message === "Successfully Authenticated") {
-        setUsername("");
-        setPassword("");
-        localStorage.setItem("isAuthenticated", true);
-        history.replace("/dashboard");
+  const login = async () => {
+    try {
+      const authenticate = await dispatch(
+        loginUser({ username: username, password: password })
+      );
+      unwrapResult(authenticate);
+      if (authenticate.payload?.msg === 'User successfully logged in') {
+        setUsername('');
+        setPassword('');
+        history.replace('/dashboard');
       }
-    });
-  }
+    } catch (err) {
+      if (err) console.log(err);
+    }
+  };
 
   // const hide = () => {
   // 	if (!'http://localhost:3000/') {
@@ -78,14 +74,15 @@ export default function Login() {
         </Button>
       </div>
       <div className="links-container">
-        <Button
-          onClick={"/register"}
-          type="button"
-          buttonStyle="btn--stealth"
-          buttonSize="btn--medium"
-        >
-          Create an account
-        </Button>
+        <Link to="/register">
+          <Button
+            type="button"
+            buttonStyle="btn--stealth"
+            buttonSize="btn--medium"
+          >
+            Create an account
+          </Button>
+        </Link>
         <Link to="/register">Register</Link>
         <Link to="/resetpw"> Reset PW</Link>
       </div>
