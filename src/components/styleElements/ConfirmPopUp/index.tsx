a7f82d89a18ee8b4811ca94c745528c9c9f505d1
Helen_Maher <helenhmaher@gmail.com>
2021-03-09 15:10:09 +0100

    formatting

--- src/components/styleElements/ConfirmPopUp/index.tsx
+++ src/components/styleElements/ConfirmPopUp/index.tsx
@@ -1,19 +1,19 @@
 import * as React from 'react';
 import styled from 'styled-components';
 
-
 interface Props {
-    display: string;
+  display: string;
 }
 
 const StyledConfirmPopUp = styled.div`
-position: fixed;
-background: var(--dark-900-50);
-z-index: 5;
-width: 100vw;
-height: 100vh;
-display:flex;
-justify-content: center;`;
+  position: fixed;
+  background: var(--dark-900-50);
+  z-index: 5;
+  width: 100vw;
+  height: 100vh;
+  display: flex;
+  justify-content: center;
+`;
 
 const Text = styled.div``;
 
@@ -21,24 +21,24 @@ const Button = styled.div``;
 
 const ButtonChoices = styled.div``;
 
-
 const PopUp = styled.div<Props>`
-display: ${(props) => props.display === 'true' ? 'flex' : 'none'};
-width: 150px;
-height: 150px;
-background: var(--prime-900);
-margin-top: 10vh;`;
+  display: ${(props) => (props.display === 'true' ? 'flex' : 'none')};
+  width: 150px;
+  height: 150px;
+  background: var(--prime-900);
+  margin-top: 10vh;
+`;
 
 export const ConfirmPopUp = (props: Props) => {
-    return(
-        <StyledConfirmPopUp>
-            <PopUp display={props.display}>
-                <Text>Confirm</Text>
-                <ButtonChoices>
-                    <Button>Yes</Button>
-                    <Button>Cancel</Button>
-                </ButtonChoices>
-            </PopUp>
-            </StyledConfirmPopUp> 
-    )
-}
+  return (
+    <StyledConfirmPopUp>
+      <PopUp display={props.display}>
+        <Text>Confirm</Text>
+        <ButtonChoices>
+          <Button>Yes</Button>
+          <Button>Cancel</Button>
+        </ButtonChoices>
+      </PopUp>
+    </StyledConfirmPopUp>
+  );
+};
